228290e4f8a50beecd06d468b334b079e25ef0a0
Saurabh <saurabhb079@gmail.com>
2026-02-24 08:24:01 +0530

    Add JavaScript practice exercises: Calculator, Reverse String, and Vowel Count

---
+++ 01Chapter/07Count_Vowels.js
@@ -0,0 +1,18 @@
+let str = "Hello World";
+
+let count = 0;
+str = str.toLowerCase();
+
+for (let i = 0; i < str.length; i++) {
+    if (
+        str[i] === "a" ||
+        str[i] === "e" ||
+        str[i] === "i" ||
+        str[i] === "o" ||
+        str[i] === "u"
+    ) {
+        count++;
+    }
+}
+
+console.log("Number of vowels:", count);
